4416f57ed6a56667b1459891eb7ed744656187d3
Katie Burdett <katie.burdett@ao.com>
2023-10-11 13:32:09 +0100

    chore: add initial test

---
+++ Tests/FizzBuzzTest.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    internal class FizzBuzzTest
+    {
+        [Test]
+        public void CanFizzBuzz()
+        {
+            var Foo = new FizzBuzzGenerator();
+            Foo.Calculate();
+            Assert.Pass();
+        }
+    }
+
+    internal class FizzBuzzGenerator
+    {
+        public void Calculate()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
